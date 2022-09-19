5a5ad9eec6700ee4cbfe2b9e484d293cd3f7a70c
pnicto <thehulk489@gmail.com>
2022-09-19 18:19:55 +0530

    Add logout route

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { login, register } from "../controllers/authController";
+import { login, register, logout } from "../controllers/authController";
 
 const router = Router();
 
 router.route("/register").post(register);
 router.route("/login").post(login);
-
+router.route("/logout").get(logout);
 export default router;
