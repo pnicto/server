627b2d0dc1ebf3fefc0c88ffde35591dadab6e12
pnicto <thehulk489@gmail.com>
2022-09-24 23:29:20 +0530

    Finish github oauth controller

---
+++ src/controllers/githubOauthController.ts
@@ -0,0 +1,80 @@
+import axios from "axios";
+import { Request, Response } from "express";
+import prisma from "../clients/prismaClient";
+import { generateJWT } from "../utils/generateJWT";
+
+const getGithubUser = async (accessToken: string) => {
+  const emails: { email: string; primary: boolean; verified: boolean }[] = (
+    await axios.get("https://api.github.com/user/emails", {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+  ).data;
+  const email = emails.find(
+    (email: { email: string; primary: boolean; verified: boolean }) =>
+      email.primary === true
+  )?.email;
+  const { login } = (
+    await axios.get("https://api.github.com/user", {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+  ).data;
+
+  return { email, login };
+};
+
+export const getUserDetails = async (req: Request, res: Response) => {
+  const { code } = req.query;
+  const postRes = await axios.post(
+    "https://github.com/login/oauth/access_token",
+    {
+      client_id: process.env.GITHUB_CLIENT_ID,
+      client_secret: process.env.GITHUB_CLIENT_SECRET,
+      code,
+    },
+    {
+      headers: {
+        Accept: "application/json",
+      },
+    }
+  );
+
+  const access_token = postRes.data["access_token"];
+  const { login, email } = await getGithubUser(access_token);
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (!user) {
+    console.log("here");
+
+    const newUser = await prisma.user.create({
+      data: {
+        email: email as string,
+        username: login,
+      },
+    });
+    console.log(newUser);
+    const accessToken = generateJWT({ userId: newUser.id });
+
+    res.cookie("accessToken", accessToken, {
+      httpOnly: true,
+      secure: true,
+    });
+  } else {
+    const accessToken = generateJWT({ userId: user.id });
+    console.log(user);
+    res.cookie("accessToken", accessToken, {
+      httpOnly: true,
+      secure: true,
+    });
+  }
+
+  res.redirect("http://localhost:3000/app");
+};
