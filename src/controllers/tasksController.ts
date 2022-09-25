a8b61e1d83672a91dfa375af63337d6a628fc995
pnicto <thehulk489@gmail.com>
2022-09-25 22:25:28 +0530

    Finish creating task in google tasks

--- src/controllers/tasksController.ts
+++ src/controllers/tasksController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import prisma from "../clients/prismaClient";
 import { StatusCodes } from "http-status-codes";
+import { oauth2Client } from "../clients/googleOauth2Client";
+import { google } from "googleapis";
 
 export const getAllTasks = async (req: Request, res: Response) => {
   const { taskcardId } = req.params;
@@ -15,6 +17,7 @@ export const getAllTasks = async (req: Request, res: Response) => {
 export const createTask = async (req: Request, res: Response) => {
   const { taskcardId } = req.params;
   const { taskTitle, description, completed } = req.body;
+
   const newTask = await prisma.task.create({
     data: {
       title: taskTitle,
@@ -29,7 +32,19 @@ export const createTask = async (req: Request, res: Response) => {
 
 export const updateTask = async (req: Request, res: Response) => {
   const { taskId } = req.params;
-  const { taskcardId, taskTitle, description, completed } = req.body;
+  const {
+    taskcardId,
+    taskTitle,
+    description,
+    completed,
+    deadlineDate,
+    eventStartDate,
+    eventEndDate,
+  } = req.body;
+  if (deadlineDate) {
+    createGoogleTask(req, taskTitle, description);
+  }
+
   const updatedTask = await prisma.task.update({
     where: {
       id: Number(taskId),
@@ -39,9 +54,56 @@ export const updateTask = async (req: Request, res: Response) => {
       taskcardId: taskcardId,
       description: description,
       completed: completed,
+      deadlineDate,
+      eventStartDate,
+      eventEndDate,
     },
   });
   res.status(StatusCodes.OK).json(updatedTask);
+};
+
+const createGoogleTask = async (
+  req: Request,
+  taskTitle: string,
+  deadlineDate: string
+) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: req.userId,
+    },
+  });
+
+  oauth2Client.setCredentials({
+    refresh_token: user?.refreshToken,
+  });
+
+  const service = google.tasks({
+    version: "v1",
+    auth: oauth2Client,
+  });
+
+  const taskLists = (await service.tasklists.list()).data.items;
+  const myTasklist = taskLists?.find(
+    (taskList) => taskList.title === "taskboard app"
+  );
+
+  if (!myTasklist) {
+    const taskListId = (
+      await service.tasklists.insert({
+        requestBody: {
+          title: "taskboard app",
+        },
+      })
+    ).data.id;
+  }
+
+  await service.tasks.insert({
+    tasklist: myTasklist?.id as string | undefined,
+    requestBody: {
+      title: taskTitle,
+      due: deadlineDate,
+    },
+  });
 };
 
 export const deleteTask = async (req: Request, res: Response) => {
