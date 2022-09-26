ba64e27b214a4cd713c2c3a00156a34be9df3e1d
pnicto <thehulk489@gmail.com>
2022-09-26 21:01:16 +0530

    Separate gcal, tasks, email to utils

--- src/controllers/tasksController.ts
+++ src/controllers/tasksController.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from "express";
 import prisma from "../clients/prismaClient";
 import { StatusCodes } from "http-status-codes";
-import { oauth2Client } from "../clients/googleOauth2Client";
-import { google } from "googleapis";
-import { SMTPClient } from "emailjs";
+import {
+  sendEmailNotification,
+  createGoogleTask,
+  createGoogleCalendarEvent,
+} from "../utils/externalServices";
 
 export const getAllTasks = async (req: Request, res: Response) => {
   const { taskcardId } = req.params;
@@ -44,11 +46,16 @@ export const updateTask = async (req: Request, res: Response) => {
   } = req.body;
 
   if (deadlineDate) {
-    createGoogleTask(req, taskTitle, description);
+    await createGoogleTask(req, taskTitle, description);
   }
 
   if (eventStartDate && eventEndDate) {
-    createGoogleCalendarEvent(req, taskTitle, eventStartDate, eventEndDate);
+    await createGoogleCalendarEvent(
+      req,
+      taskTitle,
+      eventStartDate,
+      eventEndDate
+    );
   }
   const oldTask = await prisma.task.findUnique({
     where: {
@@ -86,121 +93,13 @@ export const updateTask = async (req: Request, res: Response) => {
   });
 
   if (!(JSON.stringify(oldTask) === JSON.stringify(updatedTask))) {
-    sendEmailNotification(
+    await sendEmailNotification(
       taskboard?.boardTitle as string,
       taskboard?.sharedUsers as number[]
     );
   }
 
   res.status(StatusCodes.OK).json(updatedTask);
-};
-
-const createGoogleCalendarEvent = async (
-  req: Request,
-  taskTitle: string,
-  eventStartDate: string,
-  eventEndDate: string
-) => {
-  const user = await prisma.user.findUnique({
-    where: {
-      id: req.userId,
-    },
-  });
-
-  oauth2Client.setCredentials({
-    refresh_token: user?.refreshToken,
-  });
-
-  const service = google.calendar({
-    version: "v3",
-    auth: oauth2Client,
-  });
-
-  await service.events.insert({
-    calendarId: "primary",
-    requestBody: {
-      summary: taskTitle,
-      start: {
-        dateTime: eventStartDate,
-        timeZone: "Asia/Kolkata",
-      },
-      end: {
-        dateTime: eventEndDate,
-        timeZone: "Asia/Kolkata",
-      },
-    },
-  });
-};
-
-const createGoogleTask = async (
-  req: Request,
-  taskTitle: string,
-  deadlineDate: string
-) => {
-  const user = await prisma.user.findUnique({
-    where: {
-      id: req.userId,
-    },
-  });
-
-  oauth2Client.setCredentials({
-    refresh_token: user?.refreshToken,
-  });
-
-  const service = google.tasks({
-    version: "v1",
-    auth: oauth2Client,
-  });
-
-  const taskLists = (await service.tasklists.list()).data.items;
-  const myTasklist = taskLists?.find(
-    (taskList) => taskList.title === "taskboard app"
-  );
-
-  if (!myTasklist) {
-    const taskListId = (
-      await service.tasklists.insert({
-        requestBody: {
-          title: "taskboard app",
-        },
-      })
-    ).data.id;
-  }
-
-  await service.tasks.insert({
-    tasklist: myTasklist?.id as string | undefined,
-    requestBody: {
-      title: taskTitle,
-      due: deadlineDate,
-    },
-  });
-};
-
-const sendEmailNotification = async (
-  taskboardTitle: string,
-  sharedUsers: number[]
-) => {
-  const client = new SMTPClient({
-    user: process.env.GMAIL,
-    password: process.env.GOOGLE_APP_PASSWORD,
-    host: "smtp.gmail.com",
-    ssl: true,
-  });
-  for (const userId of sharedUsers) {
-    const user = await prisma.user.findUnique({
-      where: {
-        id: userId,
-      },
-    });
-    console.log(
-      await client.sendAsync({
-        text: `The taskboard ${taskboardTitle},which is shared to you by has some changes made by his owner.`,
-        subject: "Notification from taskboard",
-        from: "Taskboards",
-        to: user?.email as string,
-      })
-    );
-  }
 };
 
 export const deleteTask = async (req: Request, res: Response) => {
---
+++ src/utils/externalServices.ts
@@ -0,0 +1,112 @@
+import { Request } from "express";
+import prisma from "../clients/prismaClient";
+import { oauth2Client } from "../clients/googleOauth2Client";
+import { google } from "googleapis";
+import { SMTPClient } from "emailjs";
+
+export const createGoogleCalendarEvent = async (
+  req: Request,
+  taskTitle: string,
+  eventStartDate: string,
+  eventEndDate: string
+) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: req.userId,
+    },
+  });
+
+  oauth2Client.setCredentials({
+    refresh_token: user?.refreshToken,
+  });
+
+  const service = google.calendar({
+    version: "v3",
+    auth: oauth2Client,
+  });
+
+  await service.events.insert({
+    calendarId: "primary",
+    requestBody: {
+      summary: taskTitle,
+      start: {
+        dateTime: eventStartDate,
+        timeZone: "Asia/Kolkata",
+      },
+      end: {
+        dateTime: eventEndDate,
+        timeZone: "Asia/Kolkata",
+      },
+    },
+  });
+};
+
+export const createGoogleTask = async (
+  req: Request,
+  taskTitle: string,
+  deadlineDate: string
+) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: req.userId,
+    },
+  });
+
+  oauth2Client.setCredentials({
+    refresh_token: user?.refreshToken,
+  });
+
+  const service = google.tasks({
+    version: "v1",
+    auth: oauth2Client,
+  });
+
+  const taskLists = (await service.tasklists.list()).data.items;
+  const myTasklist = taskLists?.find(
+    (taskList) => taskList.title === "taskboard app"
+  );
+
+  if (!myTasklist) {
+    const taskListId = (
+      await service.tasklists.insert({
+        requestBody: {
+          title: "taskboard app",
+        },
+      })
+    ).data.id;
+  }
+
+  await service.tasks.insert({
+    tasklist: myTasklist?.id as string | undefined,
+    requestBody: {
+      title: taskTitle,
+      due: deadlineDate,
+    },
+  });
+};
+
+export const sendEmailNotification = async (
+  taskboardTitle: string,
+  sharedUsers: number[]
+) => {
+  const client = new SMTPClient({
+    user: process.env.GMAIL,
+    password: process.env.GOOGLE_APP_PASSWORD,
+    host: "smtp.gmail.com",
+    ssl: true,
+  });
+  for (const userId of sharedUsers) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    await client.sendAsync({
+      text: `The taskboard ${taskboardTitle},which is shared to you by has some changes made by his owner.`,
+      subject: "Notification from taskboard",
+      from: "Taskboards",
+      to: user?.email as string,
+    });
+  }
+};
