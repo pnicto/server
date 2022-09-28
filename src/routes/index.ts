bd489efcd21cd06761349247943e944260770ae8
pnicto <thehulk489@gmail.com>
2022-09-28 20:44:49 +0530

    Update import

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,10 +3,13 @@ import taskcardRouter from "./taskcardRouter";
 import tasksRouter from "./tasksRouter";
 import authRouter from "./authRouter";
 import githubOauthRouter from "./githubOauthRouter";
+import usersRouter from "./usersRouter";
+
 export {
   taskboardRouter,
   taskcardRouter,
   tasksRouter,
   authRouter,
   githubOauthRouter,
+  usersRouter,
 };
