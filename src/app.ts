a4cbc55ff3454a26194db624e4cd7daa016a9731
pnicto <thehulk489@gmail.com>
2022-09-29 08:11:36 +0530

    Remove unnecessary lines

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import dotenv from "dotenv";
-import { Request, Response } from "express";
 require("express-async-errors");
 
 const app = express();
@@ -10,10 +9,9 @@ dotenv.config();
 import cors from "cors";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
-import path from "path";
 
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
-app.use(express.static(path.resolve(__dirname, "..", "public")));
+
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cookieParser());
@@ -39,9 +37,7 @@ app.use("/api/taskcards", taskcardRouter);
 app.use("/api/tasks", tasksRouter);
 app.use("/api/users", usersRouter);
 app.use("/user", authRouter);
-app.get("*", async (req: Request, res: Response) => {
-  res.sendFile(path.resolve(__dirname, "..", "public", "index.html"));
-});
+
 app.use(errorHandler);
 
 app.listen(process.env.PORT, () => {
